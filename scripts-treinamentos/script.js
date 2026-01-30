7d6b5667509dd7cd2ab8f0246ea46adae3d0cb1f
genemutante <33362235+genemutante@users.noreply.github.com>
2026-01-30 13:26:01 -0300

    Update script.js

--- scripts-treinamentos/script.js
+++ scripts-treinamentos/script.js
@@ -3,8 +3,6 @@
 // =============================================================================
 import { DBHandler } from "../bd-treinamentos/db-handler.js";
 
-
-
 const icons = {
     lupa: `<svg fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>`,
     user: `<svg fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" /></svg>`,
@@ -1084,6 +1082,7 @@ window.fecharMenus = function() {
 
     tempCargoIndexParaMenu = null;
 };
+
 
 
 
