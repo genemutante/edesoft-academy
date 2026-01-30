3cf000cad678036255241286c616773623281590
genemutante <33362235+genemutante@users.noreply.github.com>
2026-01-30 18:18:57 -0300

    Update cadastro-script.js

--- scripts-treinamentos/cadastro-script.js
+++ scripts-treinamentos/cadastro-script.js
@@ -280,8 +280,7 @@ function setModoForm(mode) {
 
   // botões
   $("btnSalvar").style.display = podeEditar ? "inline-flex" : "none";
-  $("btnIrParaEdicao").style.display =
-    isAdmin && mode === "view" && currentId ? "inline-flex" : "none";
+  
 
   // excluir/desligar/reativar (via modal/botão)
   $("btnExcluir").style.display = "none";
@@ -545,4 +544,5 @@ window.buscarCep = async function buscarCep(cep) {
     console.warn("ViaCEP falhou:", e);
   }
 };
+
 
